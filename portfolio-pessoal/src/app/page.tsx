022b38e73c8b08dd3be4aa598013be02cefe0fa2
joaovictor25 <batistacarneiro1997@gmail.com>
2025-12-07 22:31:34 -0300

    Melhorias na hero

--- portfolio-pessoal/src/app/page.tsx
+++ portfolio-pessoal/src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Header } from "@/components/header";
+import { Hero } from "@/components/hero/hero";
+
 
 export default function Page(){
   return(
     <div>
       <Header></Header>
+      <Hero></Hero>
     </div>
   );
 }
